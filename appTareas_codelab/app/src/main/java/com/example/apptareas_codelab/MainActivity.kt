87834992d5fdadcc12360ec389be7724e50fe5d2
Rubense110 <rubenjj99@gmail.com>
2023-08-09 19:24:47 +0200

    Basic Task App Done

--- appTareas_codelab/app/src/main/java/com/example/apptareas_codelab/MainActivity.kt
+++ appTareas_codelab/app/src/main/java/com/example/apptareas_codelab/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.apptareas_codelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.apptareas_codelab.ui.theme.AppTareas_codelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val image = painterResource(R.drawable.ic_task_completed)
+                    Greeting(image,
+                        stringResource(R.string.title_text),
+                        stringResource(R.string.descr_text))
                 }
             }
         }
@@ -30,17 +45,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(image: Painter,
+             title: String,
+             descr: String,
+             modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Center) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = descr,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AppTareas_codelabTheme {
-        Greeting("Android")
+        val image = painterResource(R.drawable.ic_task_completed)
+        Greeting(image,
+            stringResource(R.string.title_text),
+            stringResource(R.string.descr_text))
     }
 }
